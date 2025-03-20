44bf7fd10960ea1e169a1ab6ab0ac070e6e20f51
James Pearce <jam-es123@hotmail.com>
2025-03-20 10:06:16 +0000

    moving hero out

---
+++ PASS.Web/Views/Shared/_Hero.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PASS.Web.Views.Shared
+{
+    public class _HeroModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
