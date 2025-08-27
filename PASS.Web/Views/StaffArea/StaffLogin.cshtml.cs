394c0cacfdba00a37b4eb344872165d84c6e6511
James Pearce <jam-es123@hotmail.com>
2025-08-27 14:13:02 +0100

    Login for admin and staff

---
+++ PASS.Web/Views/StaffArea/StaffLogin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PASS.Web.Views.StaffArea
+{
+    public class StaffLoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
