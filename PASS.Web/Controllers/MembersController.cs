585907f8898479a63531b608b1178791385241a6
James Pearce <jam-es123@hotmail.com>
2025-08-27 12:05:21 +0100

    working login

---
+++ PASS.Web/Controllers/MembersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace PASS.Web.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Check if user is logged in via session
+            var username = HttpContext.Session.GetString("Username");
+            ViewBag.IsLoggedIn = !string.IsNullOrEmpty(username);
+
+            // Render the same view
+            return View("~/Views/Members/MembersArea.cshtml");
+        }
+    }
+}
---
+++ PASS.Web/Models/UserAccount.cs
@@ -0,0 +1,9 @@
+﻿namespace PASS.Web.Models
+{
+    public class UserAccount
+    {
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string Role { get; set; }
+    }
+}
